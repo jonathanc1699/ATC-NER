1f5c6cb9b5dd48d16c04ce05a34b13b1765f2b03
jonathanc1699 <jonathanc1699@gmail.com>
2022-02-07 14:47:46 +0800

    cleaned folder

--- LM files/domain_corpus.py
+++ LM files/domain_corpus.py
@@ -1,7 +1,7 @@
-non_cs=1363
-num_lines=sum(1 for line in open('HotWords.txt'))
+non_cs=1372
+num_lines=sum(1 for line in open('HotWords V3.txt'))
 # open both files
-with open('HotWords.txt','r') as firstfile, open('test.txt','a') as secondfile:  
+with open('HotWords V3.txt','r') as firstfile, open('domain_corpus_5.txt','a') as secondfile:  
     # read content from first file
     for i in range(1,non_cs):
         line=firstfile.readline()
@@ -9,9 +9,9 @@ with open('HotWords.txt','r') as firstfile, open('test.txt','a') as secondfile:
         # append content to second file
         secondfile.write(line + " " + "1000\n")
     
-    for i in range(1363,num_lines):
-        line=firstfile.readline()
-        line=line.rstrip()
-        #append content to second file
-        secondfile.write(line + " " + "50\n")
+#    for i in range(1363,num_lines):
+#        line=firstfile.readline()
+#        line=line.rstrip()
+#        #append content to second file
+#        secondfile.write(line + " " + "1000\n")
     
