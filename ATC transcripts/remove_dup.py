8798e1d786147ac16c7466b302193de7af9ea13d
jonathanc1699 <jonathanc1699@gmail.com>
2022-04-05 15:00:28 +0800

    converted wav files to proper format

--- ATC transcripts/remove_dup.py
+++ ATC transcripts/remove_dup.py
@@ -1,12 +1,26 @@
 
-outfile=open("transcripts_no_dup.txt","w")
-lines_seen=set()
-with open("testset.txt","r") as f:
-    for line in f:
-        line=line.lower()
-        if not line.isspace():
-            line=line.rstrip("\n")
-            if line not in lines_seen:
-                lines_seen.add(line)
-                outfile.write(line+"\n")    
-    outfile.close()
+# outfile=open("transcripts_no_dup.txt","w")
+# lines_seen=set()
+# with open("testset.txt","r") as f:
+#     for line in f:
+#         line=line.lower()
+#         if not line.isspace():
+#             line=line.rstrip("\n")
+#             if line not in lines_seen:
+#                 lines_seen.add(line)
+#                 outfile.write(line+"\n")    
+#     outfile.close()
+
+
+import re
+import num2words
+
+
+with open('callsign2.txt') as f_input:
+    text = f_input.read()
+
+
+text = re.sub(r"(\d+)", lambda x: num2words.num2words(int(x.group(0))), text)
+
+with open('callsign2_words.txt', 'w') as f_output:
+    f_output.write(text)
